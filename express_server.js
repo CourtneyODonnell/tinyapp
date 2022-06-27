eed639e51e981a5e2f47d2158566fddb57cfce2c
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-26 22:39:13 -0300

    fix route

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
 const bodyParser = require("body-parser");
+
 const { request } = require("express");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
