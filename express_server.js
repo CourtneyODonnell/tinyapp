b1518f55e49fc62592ffe35f94ec6739933d7762
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-07-01 16:38:12 -0300

    route login

--- express_server.js
+++ express_server.js
@@ -28,15 +28,19 @@ const generateRandomString = function(length) {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//cookie parser require
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 //add GET route to show the form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies['username']};
+  res.render("urls_new", templateVars);
 });
 
 //add new route
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies['username']};
   res.render('urls_show', templateVars);
   //adding shorturl route
 });
@@ -59,7 +63,7 @@ app.get("/hello", (req, res) => {
 
 //urls_index
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 });
 
@@ -101,4 +105,10 @@ app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.updatedURL;
   res.redirect(`/urls/${shortURL}`);
+});
+
+//login
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('urls');
 });
