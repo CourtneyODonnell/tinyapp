c6fa8e4e134c2526346422f8c30ef550f7cf8932
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-07-03 23:05:28 -0300

    final big code clean-up

--- express_server.js
+++ express_server.js
@@ -1,16 +1,16 @@
-//HASH & ENCRYPTION
-//replace cookie parser with cookie session
 const cookieSession = require('cookie-session');
-//bcryptjs
 const bcrypt = require('bcryptjs');
 const getUserByEmail = require('./helpers');
-
-//CONSTANTS
 const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
-//getting ready for post requests by adding body parser
 const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8080;
+const users = {};
+
+app.set("view engine", "ejs");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
@@ -20,19 +20,7 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
 
-//listen
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-
-app.set("view engine", "ejs");
-
-//CONST OBJECTS/DATABASES
-
-//create user object
-const users = {};
-
+//const objects and functions
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -44,37 +32,19 @@ const urlDatabase = {
   }
 };
 
-//CONST HELPER FUNCTIONS
-
-//Generate a Random ShortURL
+//function to generate random characters based on given length parameter
 const generateRandomString = function(length) {
-  //variable for characters
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  //variable to create random string
   let result = '';
   let charactersLength = characters.length;
-  //for loop
   for (let i = 0; i < length; i++) {
-    //original empty string + random characters from characters with math floor to produce whole integers
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 };
 
-// //email already registered helper function
-// const searchForEmail = (email) => {
-//   //for in loop to scan object
-//   for (const user in users) {
-//     //if input email is already registered in database, then...
-//     if (users[user].email === email) {
-//       return users[user];
-//     }
-//   }
-//   return false;
-// };
-
+//returns URLs where userID is equal to the id of the currently logged-in user
 const urlsForUser = (id) => {
-  //returns URLs where userID is equal to the id of the currently logged-in user
   let result = {};
   for (let URLid of Object.keys(urlDatabase)) {
     if (urlDatabase[URLid].userID === id) {
@@ -86,10 +56,8 @@ const urlsForUser = (id) => {
 
 //ROUTING
 
-//add GET route to show the form
 app.get("/urls/new", (req, res) => {
   let templateVars = {user: users[req.session['user_id']]};
-  //Modify so that only registered & logged in users can create new tiny URLs.
   if (!templateVars.user) {
     res.status(400).send("This feature is only accessible when logged in");
     return;
@@ -97,7 +65,7 @@ app.get("/urls/new", (req, res) => {
   return res.render('urls_new', templateVars);
 });
 
-//add new route
+//new URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
@@ -113,14 +81,17 @@ app.get("/urls/:shortURL", (req, res) => {
   }
 });
 
+//main page greeting
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//json of urls
 app.get("/urls.json", (req, res) => {
   res.send(users);
 });
 
+//hello world page
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -134,17 +105,15 @@ app.get("/urls", (req, res) => {
   if (templateVars.user) {
     res.render("urls_index", templateVars);
   } else {
-    res.status(400).send("Please log in to access this feature.");
+    res.status(400).send("Please log in at http://localhost:8080/login to access this feature.");
   }
 });
 
-//POST
-//define the route that will match this POST request and handle it
+//POST /urls
 app.post("/urls", (req, res) => {
-  //should not display URLs unless the user is logged in. It should instead display a message or prompt suggesting that they log in or register first
   const userID = req.session['user_id'];
   const longURL = req.body.longURL;
-  const shortURL = generateRandomString(6); //variable for shortURL calling generateRandomString function
+  const shortURL = generateRandomString(6);
   urlDatabase[shortURL] = {
     longURL,
     userID
@@ -153,32 +122,29 @@ app.post("/urls", (req, res) => {
 });
 
 
-//redirect short URLs
+//:shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  //if longURL is truthy, redirect to
   if (longURL) {
     res.redirect(urlDatabase[req.params.shortURL].longURL);
   } else {
-    //redirect to 404 error
     res.statusCode = 404;
     res.send('<h2>404 Not Found<br>This shortURL does not exist!</h2>');
   }
 });
 
-//Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
+//delete shortURL
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   if (req.session['user_id'] === urlDatabase[shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
     res.redirect('/urls');
-    //redirects to main url page
   } else {
     res.status(400).send("You do not have permission to delete this URL");
   }
 });
 
-//update longURL in database
+//update longURL
 app.post('/urls/:shortURL', (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = req.params.shortURL;
@@ -190,23 +156,21 @@ app.post('/urls/:shortURL', (req, res) => {
   }
 });
 
-//logout POST
+//POST logout
 app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
 
-//registration GET
+//GET register
 app.get('/register', (req, res) => {
   let templateVars = {user: users[req.session['user_id']]};
   res.render('urls_registration', templateVars);
 });
 
-//registration handler POST
+//POST registration handler
 app.post('/register', (req, res) => {
-  //if req body email and pw truthy, then, if not in database, then...
   if (req.body.email && req.body.password) {
-    //if (!searchForEmail(req.body.email)) {
     if (!getUserByEmail(req.body.email, users)) {
       const userID = generateRandomString(5);
       users[userID] = {
@@ -227,15 +191,14 @@ app.post('/register', (req, res) => {
   }
 });
 
-//LOGIN get
+//GET login
 app.get('/login', (req, res) => {
   let templateVars = {user: users[req.session['user_id']]};
   res.render('urls_login', templateVars);
 });
 
-//login handler POST
+//POST login handler
 app.post('/login', (req, res) => {
-  // const user = searchForEmail(req.body.email);
   const user = getUserByEmail(req.body.email, users);
  
   if (user) {
